97a3326c9079518a9a5469e4cbdd42aaf9feae65
Chenyang Zhong <zhongcy95@gmail.com>
2021-03-11 14:53:39 -0500

    dm-table: re-arrange inlinecrypt capability check logic
    According to upstream commit 75c5d315, "a non-zero return code
    returned by iterate_devices_callout_fn will stop the iteration in
    advance". In addition, commit a36aeade specifically inverts the
    dax capability check to be is(not capable) instead of not(capable)
    to address the issue. Therefore, follow that change to invert the
    logic on inlinecrypt capability check as well.
    
    [Dhina17] : Adapted
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: Ia6ca5aed174e781017299d626676e30f787d38a6

--- drivers/md/dm-table.c
+++ drivers/md/dm-table.c
@@ -1541,14 +1541,14 @@ static int queue_no_sg_merge(struct dm_target *ti, struct dm_dev *dev,
 	return q && test_bit(QUEUE_FLAG_NO_SG_MERGE, &q->queue_flags);
 }
 
-static int queue_supports_inline_encryption(struct dm_target *ti,
+static int queue_not_inline_encryption(struct dm_target *ti,
 					    struct dm_dev *dev,
 					    sector_t start, sector_t len,
 					    void *data)
 {
 	struct request_queue *q = bdev_get_queue(dev->bdev);
 
-	return q && blk_queue_inlinecrypt(q);
+	return q && !blk_queue_inlinecrypt(q);
 }
 
 
@@ -1655,10 +1655,10 @@ void dm_table_set_restrictions(struct dm_table *t, struct request_queue *q,
 	else
 		queue_flag_clear_unlocked(QUEUE_FLAG_NO_SG_MERGE, q);
 
-	if (dm_table_all_devices_attribute(t, queue_supports_inline_encryption))
-		queue_flag_set_unlocked(QUEUE_FLAG_INLINECRYPT, q);
-	else
+	if (dm_table_any_dev_attr(t, queue_not_inline_encryption))
 		queue_flag_clear_unlocked(QUEUE_FLAG_INLINECRYPT, q);
+	else
+		queue_flag_set_unlocked(QUEUE_FLAG_INLINECRYPT, q);
 
 	dm_table_verify_integrity(t);
 
